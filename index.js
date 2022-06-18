9b8eab0bbfb2f2ab8708f380ef6650c51a3f9d00
John Kiely <john@Johns-MacBook-Pro.local>
2022-06-18 17:06:55 +0100

    Working chat function to demo websockets

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
@@ -10,22 +10,20 @@ const io = new Server(server);
 // // const __filename = url.fileURLToPath(import.meta.url);
 // const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/test.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/test.html");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  })
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
-
-
 
 // import * as url from 'url';
 
@@ -35,5 +33,5 @@ server.listen(3000, () => {
 // app.get('/', (req, res) => {
 //   app.use(express.static(__dirname + "/public"))
 //   res.sendFile(__dirname + '/index.html')
-  
+
 // })
